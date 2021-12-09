0aac8a66b9c401f8235c305aa5c5a84b17e6c564
diego <diegoteixeirapereira@hotmail.com>
2021-12-09 16:46:05 -0300

    criação do arquivo classe principal.

--- -01-CalcularValorTroco/src/br/com/java/CalcularValorTroco.java
+++ -01-CalcularValorTroco/src/br/com/java/CalcularValorTroco.java
@@ -1,9 +1,24 @@
 package br.com.java;
 
+import java.util.Scanner;
+
 public class CalcularValorTroco {
 	
 	public static void main(String[] args) {
 		
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.print("Digite o valor do produto: ");
+		Double valorProduto = teclado.nextDouble();
+		
+		System.out.print("Digite valor pago pelo cliente: ");
+		Double valorPago = teclado.nextDouble();
+		
+		Double resultado = valorProduto - valorPago;
+		
+		System.out.println("Troco: " + resultado);
+		
+		teclado.close();
 	}
 
 }
