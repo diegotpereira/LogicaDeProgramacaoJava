1f5f13793dd3a4b2577b597fbb1db53295ae071f
diego <diegoteixeirapereira@hotmail.com>
2021-12-10 09:14:08 -0300

    criação do app console java com exemplo de Concatenacao de textos.

---
+++ -10-ConcatenacaoTextos/src/br/com/java/ConcatenacaoTextos.java
@@ -0,0 +1,17 @@
+package br.com.java;
+
+public class ConcatenacaoTextos {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		String inicio = "Esse é o ";
+		String meio = "º exemplo ";
+		String fim = "sobre concatenação de textos.";
+		Integer numeroExemplo = 1;
+		
+		System.out.println(inicio + numeroExemplo + meio + fim);
+
+	}
+
+}
