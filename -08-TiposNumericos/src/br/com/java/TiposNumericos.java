3211d1963c53f1a4d28f45e09fd40d1ae4ab7d32
diego <diegoteixeirapereira@hotmail.com>
2021-12-10 08:59:09 -0300

    criação do app console java com exemplo de tipos númericos.

---
+++ -08-TiposNumericos/src/br/com/java/TiposNumericos.java
@@ -0,0 +1,29 @@
+package br.com.java;
+
+public class TiposNumericos {
+
+	public static void main(String[] args) {
+
+        Byte numeroByte = 127; // -128 até 127
+		
+		Short numeroShort = -32768; // -32768 até 32767
+		
+		Integer numeroInteger = 2147483647; // -2147483648 até 2147483647
+		
+		Long numeroLong = 1L; // -9,223372037×10¹⁸ até 9,223372037×10¹⁸
+		
+		Float numeroFloat = 1.0f;
+		
+		Double numeroDouble = 1.0;
+		
+		System.out.println("os tipos número são " 
+		                  + "\nByte: " + numeroByte
+		                  + "\nShort: " + numeroShort
+		                  + "\nInteger: " + numeroInteger
+		                  + "\nLong: " + numeroLong
+		                  + "\nFloat: " + numeroFloat
+		                  + "\nDouble: " + numeroDouble);
+
+	}
+
+}
