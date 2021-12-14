dfcdc7de89e47556b2829b91bfc3daeccd81ad81
diegotpereira <diegoteixeirapereira@hotmail.com>
2021-12-14 15:35:33 -0300

    criação do app console java com exemplo de Vetores1Dimensao.

---
+++ -21-Vetores1Dimensao/src/br/com/java/Vetores1Dimensao.java
@@ -0,0 +1,19 @@
+package br.com.java;
+
+public class Vetores1Dimensao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] cardapio = new String[4];
+		cardapio[0] = "Calabresa";
+		cardapio[1] = "Atum";
+		cardapio[2] = "Queijo";
+		cardapio[3] = "Presunto";
+		
+		for(int i =0; i < cardapio.length; i++) {
+			System.out.println(cardapio[i]);
+		}
+				
+	}
+
+}
